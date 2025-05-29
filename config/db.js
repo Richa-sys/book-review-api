a3d3bd2346f6ce7669b49323cb1fcddc0067f17d
Richa-sys <ruhela.richa1@gmail.com>
2025-05-30 00:19:31 +0530

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose');
 
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useFindAndModify: false, // deprecated in mongoose 6
+      // useCreateIndex: true // deprecated in mongoose 6
+    });
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting DB: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
